add19db87751b31b152881839306393954ee53cb
Hans <126028768+HansVanHousen@users.noreply.github.com>
2023-04-13 21:16:14 -0700

    duplicate files made to mess around with some alternate html/css/js script.

---
+++ assets/quiz.js
@@ -0,0 +1,32 @@
+
+const mainlist = [ 
+    {
+        question: "who is the captain of NCC-1701-D",
+        answers: ["Kirk", "Picard", "Janeway", "Sisko"],
+        correct: "Picard",
+    },
+
+    {
+        questions: "who is 2nd in command on DS9",
+        answers: ["Worf", "Riker", "Kira"],
+        correct: "Kira"
+    },
+
+    {
+        questions: "If you have a Horga'hn on display, what do you seek?",
+        answers: ["The Celestial Temple", "Jamaharon", "A Borg Cube"],
+        correct: "Jamaharon"
+    },
+
+    {
+        questions: "What has Voyager gone too far to be stopped by?",
+        answers: ["Dust", "The Borg", "Lack of Dilithium Crystals"],
+        correct: "Dust"
+    },
+
+    {
+        questions: "What class is the USS Cerritos?",
+        answers: ["Galaxy", "California", "Dreadnaught"],
+        correct: "California"
+    },
+]
